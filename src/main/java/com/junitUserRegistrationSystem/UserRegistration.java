a23a31165cca3137bea84df6314c8faadcac1478
Your Name <you@example.com>
2020-10-27 12:37:56 +0530

    Added: email Method

--- src/main/java/com/junitUserRegistrationSystem/UserRegistration.java
+++ src/main/java/com/junitUserRegistrationSystem/UserRegistration.java
@@ -12,4 +12,9 @@ public class UserRegistration {
 		boolean lname = Pattern.matches("^[A-Z]([a-z]{2,})$", lastName);
 		return lname;
 	}
+
+	public static boolean email(String mail) {
+		boolean email = Pattern.matches("^[A-Za-z0-9]+([._%+-][0-9a-zA-Z]+)*@[A-Za-z0-9]+([.]([a-zA-Z]{2,3})*)+$", mail);
+		return email;
+	}
 }
