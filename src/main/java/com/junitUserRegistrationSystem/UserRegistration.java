cb802ebb6e5314b052b10178a8aaf9d1b0c35401
Your Name <you@example.com>
2020-10-27 13:03:09 +0530

    Added: mobileNum method

--- src/main/java/com/junitUserRegistrationSystem/UserRegistration.java
+++ src/main/java/com/junitUserRegistrationSystem/UserRegistration.java
@@ -14,7 +14,12 @@ public class UserRegistration {
 	}
 
 	public static boolean email(String mail) {
-		boolean email = Pattern.matches("^[A-Za-z0-9]+([._%+-][0-9a-zA-Z]+)*@[A-Za-z0-9]+([.]([a-zA-Z]{2,3})*)+$", mail);
+		boolean email = Pattern.matches("^[A-Za-z0-9]+([._%+-][0-9a-zA-Z]+)*@[A-Za-z0-9]+([.]([a-zA-Z]{2,3})*)+$",mail);
 		return email;
+	}
+
+	public static boolean mobileNum(String mNum) {
+		boolean mobileNum = Pattern.matches("^[+]*([0-9]{1,3})+[[:space:]]+[0-9]{10}$", mNum);
+		return mobileNum;
 	}
 }
