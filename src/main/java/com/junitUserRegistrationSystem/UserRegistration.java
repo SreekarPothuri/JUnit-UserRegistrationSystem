f5275ed076ef7938207d417876cfdcc7012ae2e6
Your Name <you@example.com>
2020-10-27 12:16:04 +0530

    Added: firstName Method

--- src/main/java/com/junitUserRegistrationSystem/UserRegistration.java
+++ src/main/java/com/junitUserRegistrationSystem/UserRegistration.java
@@ -1,5 +1,10 @@
 package com.junitUserRegistrationSystem;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-	
+	public static boolean firstName(String firstName) {
+		boolean fname = Pattern.matches("^[A-Z]([a-z]{2,})$", firstName);
+		return fname;
+	}
 }
